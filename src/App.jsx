727b0277f7289c4eff9bc55db3d30f4e85eca548
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-11-01 19:43:51 +0700

    add homepage header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-4xl font-semibold text-center text-blue-700'>
-        Hello World!
-      </h1>
+      <Nav />
+      <div className='h-[100vh] pt-[133.383px] px-64'>
+        {/* header */}
+        <header className='py-8'>
+          <h1 className='text-4xl font-semibold'>Selamat Datang di Kos Mohe</h1>
+          {/* diskon */}
+          <div className='flex gap-10 mt-10'>
+            <div className='w-1/2 p-5 bg-[#F3F4FF] rounded-2xl'>
+              <h2 className='text-2xl'>Kamar Deluxe</h2>
+              <h3 className='mt-2 text-5xl font-semibold text-primary'>
+                20% OFF
+              </h3>
+              <p className='mt-10 text-[#83859C]'>1 - 30 Januari 2023</p>
+            </div>
+            <div className='w-1/2 p-5 bg-[#FFF3ED] rounded-2xl'>
+              <h2 className='text-2xl'>Kamar Reguler</h2>
+              <h3 className='mt-2 text-5xl font-semibold text-[#FD6D22]'>
+                10% OFF
+              </h3>
+              <p className='mt-10 text-[#83859C]'>1 - 15 Januari 2023</p>
+            </div>
+          </div>
+          {/* kamar kos */}
+        </header>
+      </div>
+      <Footer />
     </div>
   );
 };
