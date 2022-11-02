eb1facdeb8f76c543875d21fd1147ac981c8fa58
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-11-02 13:51:21 +0700

    implement home fitur view limitation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -88,36 +88,40 @@ const Home = ({ kamar, fasilitas }) => {
           <h2 className='text-xl'>Fasilitas Kos Mohe</h2>
           {/* content */}
           <div className='flex flex-wrap gap-8 mt-6'>
-            {fasilitas.map((item) => (
-              <div className='flex gap-4'>
-                <img
-                  className='w-64 rounded-2xl'
-                  src={`${item.img}`}
-                  alt={`${item.title}`}
-                />
-                <div>
-                  <h3 className='text-lg font-semibold'>{item.title}</h3>
-                  <p className='text-sm'>{item.description}</p>
-                  {item.title === "Kamar Tidur" ? (
-                    <ul className='grid grid-cols-2 grid-rows-4 ml-5 text-sm list-disc'>
-                      {item.features.map((feature) => (
-                        <li>{feature}</li>
-                      ))}
-                    </ul>
-                  ) : (
-                    <ul className='ml-5 text-sm list-disc'>
-                      {item.features.map((feature) => (
-                        <li>{feature}</li>
-                      ))}
-                    </ul>
-                  )}
-                </div>
-              </div>
-            ))}
+            {fasilitas.map((item, i) => {
+              if (i < 2) {
+                return (
+                  <div className='flex gap-4'>
+                    <img
+                      className='w-64 rounded-2xl'
+                      src={`${item.img}`}
+                      alt={`${item.title}`}
+                    />
+                    <div>
+                      <h3 className='text-lg font-semibold'>{item.title}</h3>
+                      <p className='text-sm'>{item.description}</p>
+                      {item.title === "Kamar Tidur" ? (
+                        <ul className='grid grid-cols-2 grid-rows-4 ml-5 text-sm list-disc'>
+                          {item.features.map((feature) => (
+                            <li>{feature}</li>
+                          ))}
+                        </ul>
+                      ) : (
+                        <ul className='ml-5 text-sm list-disc'>
+                          {item.features.map((feature) => (
+                            <li>{feature}</li>
+                          ))}
+                        </ul>
+                      )}
+                    </div>
+                  </div>
+                );
+              }
+            })}
           </div>
           {/* learn more button */}
           <a
-            href='/fasilitas'
+            href='/fitur'
             className='inline-block px-8 py-2 mt-8 font-semibold text-white transition-colors duration-150 hover:bg-primary-light rounded-2xl bg-primary'
           >
             Learn More
