015b27a17899bb7cbb4dd53733178e85ade349f7
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-11-02 13:24:35 +0700

    implement kamar page

--- src/pages/Kamar.jsx
+++ src/pages/Kamar.jsx
@@ -1,17 +1,57 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
+import { CiUser } from "react-icons/ci";
+import { BsDot } from "react-icons/bs";
 
-const Kamar = () => {
+const Kamar = ({ kamar }) => {
   return (
     <>
       <Nav />
       <div className='min-h-[100vh] pt-[85.0667px] px-48'>
         {/* header */}
-        <header className='py-6'>
+        <header className='pt-6'>
           {/* title */}
           <h1 className='text-3xl'>Kamar Kos Mohe</h1>
         </header>
+        <section id='kamar' className='py-6'>
+          {/* content */}
+          <div className='grid grid-cols-3 pb-6 gap-x-4 gap-y-6'>
+            {kamar.map((item) => (
+              <a
+                href={item.url}
+                className='duration-75 border-[1px] transform rounded-2xl hover:scale-[101%]'
+              >
+                <div className='relative'>
+                  <img
+                    src={`${item.img}`}
+                    alt={`${item.title}`}
+                    className='rounded-t-2xl'
+                  />
+                  {item.featured && (
+                    <div className='absolute top-[1px] right-[1px] px-2 py-1 text-sm text-white bg-primary rounded-bl-2xl rounded-tr-2xl'>
+                      Featured
+                    </div>
+                  )}
+                </div>
+                <div className='p-4'>
+                  <h3 className='text-lg font-semibold'>{item.title}</h3>
+                  <p className='text-sm'>
+                    <CiUser className='inline' /> {item.people} orang{" "}
+                    <BsDot className='inline' /> Rp {item.price}
+                  </p>
+                  <ul className='flex flex-wrap gap-2 mt-5 text-sm'>
+                    {item.features.map((feature) => (
+                      <li className='px-3 py-1 bg-[#EDEEF2] rounded-xl'>
+                        {feature}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </a>
+            ))}
+          </div>
+        </section>
       </div>
       <Footer />
     </>
