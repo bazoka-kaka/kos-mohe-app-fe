b9407a99278e7aba988fa2f4f74240ce07adcfaa
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-11-13 23:56:18 +0700

    change login request route

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -12,7 +12,7 @@ import axios from "../../api/axios";
 
 const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-const LOGIN_URL = "/login";
+const LOGIN_URL = "/auth";
 
 const Login = () => {
   // hide password
