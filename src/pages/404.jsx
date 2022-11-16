820419d18ede19e2f6f11c23651bd2d60f0aacc4
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-11-16 14:22:26 +0700

    implement go back button in 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import Footer from "../components/Footer";
-import Nav from "../components/Nav";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => navigate(-1);
   return (
     <>
-      <Nav />
       <div className='min-h-[100vh] pt-[85.0667px] px-48 flex items-center justify-center'>
         <div className='flex flex-col items-center gap-6'>
           <h1 className='text-4xl'>404 Page Not Found!</h1>
           <div>
-            <a
-              href='/'
+            <button
+              onClick={goBack}
               className='inline px-12 py-2 text-sm font-semibold text-white transition-colors duration-150 hover:bg-primary-light rounded-2xl bg-primary'
             >
               Home
-            </a>
+            </button>
           </div>
         </div>
       </div>
-      <Footer />
     </>
   );
 };
