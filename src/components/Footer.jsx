a1c831037153f5ac85d6546de7242b4eaf628bf5
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-11-25 14:40:42 +0700

    add Footer logics

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const Footer = () => {
+  const { auth } = useAuth();
   return (
     <footer className='relative flex justify-between px-48 py-12 z-5 bg-primary'>
       <div className='flex items-center'>
@@ -21,21 +23,41 @@ const Footer = () => {
             <li>
               <Link to='/fitur'>Fitur</Link>
             </li>
-            <li>
-              <Link to='/kontak'>Kontak</Link>
-            </li>
+            {auth.id && (
+              <li>
+                <Link to='/kontak'>Kontak</Link>
+              </li>
+            )}
           </ul>
         </div>
         {/* authentication */}
         <div className='tracking-wide'>
-          <p className='font-bold'>Authentication</p>
+          <p className='font-bold'>
+            {auth.id ? "Dashboard" : "Authentication"}
+          </p>
           <ul className='text-white'>
-            <li>
-              <Link to='/register'>Register</Link>
-            </li>
-            <li>
-              <Link to='/login'>Login</Link>
-            </li>
+            {!auth.id ? (
+              <>
+                <li>
+                  <Link to='/register'>Register</Link>
+                </li>
+                <li>
+                  <Link to='/login'>Login</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to='/dashboard'>Profile</Link>
+                </li>
+                <li>
+                  <Link to='/payment'>Payment</Link>
+                </li>
+                <li>
+                  <Link to='/security'>Security</Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
