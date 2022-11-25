475785fdebfe221c572041219d09d22a4968e517
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-11-25 18:28:26 +0700

    add payment logics

--- src/pages/Client/Dashboard/Payment.jsx
+++ src/pages/Client/Dashboard/Payment.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "../../../components/Sidebar";
 import { useNavigate } from "react-router-dom";
 import useLogout from "../../../hooks/useLogout";
+import useAuth from "../../../hooks/useAuth";
+import axios from "../../../api/axios";
+
+const ORDERS_URL = "/orders";
 
 const Payment = () => {
+  const { auth } = useAuth();
+
   const navigate = useNavigate();
   const logout = useLogout();
 
@@ -11,6 +17,23 @@ const Payment = () => {
     await logout();
     navigate("/");
   };
+
+  const [orders, setOrders] = useState([]);
+
+  const getOrders = async () => {
+    try {
+      const response = await axios.get(ORDERS_URL + "/" + auth.id);
+      console.log(response?.data);
+      setOrders(response?.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
   return (
     <>
       <div className='min-h-[100vh] pt-[85.0667px] flex px-48'>
@@ -24,23 +47,32 @@ const Payment = () => {
               Pembayaran Belum Lunas
             </h2>
             {/* cards */}
-            <div className='flex flex-col mt-6'>
-              <div className='flex px-12 py-2 rounded-xl justify-between border-[1px] border-slate-200'>
-                <div className='flex flex-col gap-2'>
-                  <h3 className='font-semibold text-slate-700'>Deluxe</h3>
-                  <p className='text-sm text-slate-500'>2 Bulan x Rp 900.000</p>
-                  <p className='text-sm text-slate-500'>
-                    1 Juli 2022 - 31 Agustus 2022
-                  </p>
-                </div>
-                <div className='flex flex-col items-end justify-center'>
-                  <button className='px-4 py-1 text-white bg-black rounded-md'>
-                    Bayar
-                  </button>
-                  <p className='mt-2 text-sm'>Total: Rp 1.800.000</p>
+            {orders.map((order) => (
+              <div className='flex flex-col mt-6'>
+                <div className='flex px-12 py-2 rounded-xl justify-between border-[1px] border-slate-200'>
+                  <div className='flex flex-col gap-2'>
+                    <h3 className='font-semibold text-slate-700'>
+                      {order.name}
+                    </h3>
+                    <p className='text-sm text-slate-500'>
+                      {order.duration} Bulan
+                    </p>
+                    <p className='text-sm text-slate-500'>
+                      {order.begin_date.substring(0, 10)} s.d{" "}
+                      {order.end_date.substring(0, 10)}
+                    </p>
+                  </div>
+                  <div className='flex flex-col items-end justify-center'>
+                    <button className='px-4 py-1 text-white bg-black rounded-md'>
+                      Bayar
+                    </button>
+                    <p className='mt-2 text-sm'>
+                      Total: Rp {order.total_price}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
 
             {/* cta */}
             <div className='flex justify-between mt-8'>
