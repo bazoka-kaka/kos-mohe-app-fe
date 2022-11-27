126cee945b8c84a27cbdcbdef84a814d4241a2df
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-11-28 06:18:04 +0700

    add reverse notifications implementation

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -10,6 +10,7 @@ const Notifications = ({ notifications, setNotifications }) => {
   const { auth } = useAuth();
 
   const [maxi, setMaxi] = useState(4);
+  const [reversed, setReversed] = useState([]);
 
   const addMaxi = () => {
     if (maxi < notifications.length) {
@@ -17,11 +18,18 @@ const Notifications = ({ notifications, setNotifications }) => {
     }
   };
 
+  useEffect(() => {
+    const temp = [...notifications];
+    temp.reverse();
+    setReversed(temp);
+  }, []);
+
   const deleteNotifications = async () => {
     try {
       const response = await axios.delete(NOTIFICATIONS_URL + "/" + auth.id);
       console.log(response?.data);
       setNotifications([]);
+      setReversed([]);
     } catch (err) {
       console.log(err);
     }
@@ -31,7 +39,7 @@ const Notifications = ({ notifications, setNotifications }) => {
     <div className='min-h-[100vh] flex flex-col pt-[85.0667px] pb-6 px-48'>
       <div className='flex justify-between mt-6'>
         <h1 className='text-3xl font-semibold'>Notifikasi</h1>
-        {notifications.length > 0 && (
+        {reversed.length > 0 && (
           <button
             onClick={deleteNotifications}
             className='font-semibold text-blue-600 transition duration-200 hover:text-red-600'
@@ -41,10 +49,10 @@ const Notifications = ({ notifications, setNotifications }) => {
         )}
       </div>
       <div className='flex flex-col mt-6 gap-[1px]'>
-        {notifications.length === 0 ? (
+        {reversed.length === 0 ? (
           <p>No notifications found!</p>
         ) : (
-          notifications.map((notification, i) => {
+          reversed.map((notification, i) => {
             if (i < maxi)
               return (
                 <div key={i} className='flex gap-4 px-6 py-2 bg-blue-100'>
@@ -67,7 +75,7 @@ const Notifications = ({ notifications, setNotifications }) => {
         )}
       </div>
       <div className='mt-2'>
-        {maxi < notifications.length && (
+        {maxi < reversed.length && (
           <button
             onClick={addMaxi}
             className='px-2 py-1 text-white bg-black rounded-md hover:bg-slate-700'
